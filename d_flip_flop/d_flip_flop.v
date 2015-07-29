28233322673daea05fc9940c3655b2aedb504193
nadenc <plesanc@nadesa.ca>
2015-07-28 21:40:15 -0700

    Flip Flop Cleanup
    removed excess in flip flop modules

--- d_flip_flop/d_flip_flop.v
+++ d_flip_flop/d_flip_flop.v
@@ -1,19 +1,3 @@
-//`#start header` -- edit after this line, do not edit this line
-// ========================================
-//
-// Copyright YOUR COMPANY, THE YEAR
-// All Rights Reserved
-// UNPUBLISHED, LICENSED SOFTWARE.
-//
-// CONFIDENTIAL AND PROPRIETARY INFORMATION
-// WHICH IS THE PROPERTY OF your company.
-//
-// ========================================
-`include "cypress.v"
-//`#end` -- edit above this line, do not edit this line
-
-//`#start body` -- edit after this line, do not edit this line
-
 module d_flip_flop (
 input   data    ,
 input   clk     ,
@@ -25,11 +9,3 @@ always @ (negedge clk) begin
 end
 
 endmodule
-
-//`#end` -- edit above this line, do not edit this line
-
-//`#start footer` -- edit after this line, do not edit this line
-//`#end` -- edit above this line, do not edit this line
-
-
-//[] END OF FILE
